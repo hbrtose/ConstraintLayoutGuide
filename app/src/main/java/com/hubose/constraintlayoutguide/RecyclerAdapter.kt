c5b1cc9479af97df6919ee7a234547c197b3d162
Hubert Osewski (HBOE) <hboe@demant-technology.com>
2019-05-28 09:32:46 +0200

    header & footer

--- app/src/main/java/com/hubose/constraintlayoutguide/RecyclerAdapter.kt
+++ app/src/main/java/com/hubose/constraintlayoutguide/RecyclerAdapter.kt
@@ -3,6 +3,7 @@ package com.hubose.constraintlayoutguide
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.task_fragment.view.*
 
