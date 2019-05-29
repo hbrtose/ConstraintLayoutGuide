ea8981a31fa204fd1a6656af4b394f0485e0a778
Hubert Osewski (HBOE) <hboe@demant-technology.com>
2019-05-29 14:36:22 +0200

    finished practice

--- app/src/main/java/com/hubose/constraintlayoutguide/RecyclerAdapter.kt
+++ app/src/main/java/com/hubose/constraintlayoutguide/RecyclerAdapter.kt
@@ -6,8 +6,9 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.task_fragment.view.*
+import java.lang.RuntimeException
 
-class RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.RecyclerHolder>() {
+class RecyclerAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     companion object {
         const val HEADER = 0
@@ -15,23 +16,39 @@ class RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.RecyclerHolder>() {
         const val FOOTER = 2
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.task_fragment, parent, false)
-        return RecyclerHolder(view)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when(viewType) {
+            HEADER -> HeaderHolder(LayoutInflater.from(parent.context).inflate(R.layout.header_item, parent, false))
+            FOOTER -> FooterHolder(LayoutInflater.from(parent.context).inflate(R.layout.footer_item, parent, false))
+            ITEM -> RecyclerHolder(LayoutInflater.from(parent.context).inflate(R.layout.task_fragment, parent, false))
+            else -> throw RuntimeException("Unknown view type")
+        }
     }
 
     override fun getItemCount(): Int {
         return 10
     }
 
-    override fun onBindViewHolder(holder: RecyclerHolder, position: Int) {
-        holder.bind(position)
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if(holder is RecyclerHolder) {
+            holder.bind(position)
+        }
     }
 
+    override fun getItemViewType(position: Int): Int {
+        return when(position){
+            0 -> HEADER
+            9 -> FOOTER
+            else -> ITEM
+        }
+    }
 
     class RecyclerHolder(private val view: View): RecyclerView.ViewHolder(view){
         fun bind(number: Int) {
             view.textView8.text = number.toString()
         }
     }
+
+    class HeaderHolder(view: View): RecyclerView.ViewHolder(view)
+    class FooterHolder(view: View): RecyclerView.ViewHolder(view)
 }
